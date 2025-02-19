134d1737e3105e0851cba7730c8be3d086305c53
Ivan <chaca360@gmail.com>
2025-02-18 20:02:34 -0500

    fix: route
    ....

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, HashRouter } from "react-router";
 import { AppRouter } from "./AppRouter.jsx";
+
+const basename = import.meta.env.MODE === "production" ? "/your_store_frontend" : "/";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-   <BrowserRouter basename="/your_store_frontend">
+   <HashRouter basename="">
       <AppRouter/>
-    </BrowserRouter>
+    </HashRouter>
 );
